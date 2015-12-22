3516c972d5a84b9dc5d1249432a9d74a23cd213f
dgriego <daniel@jackrabbitsystems.com>
2015-12-22 14:54:50 -0700

    adds lesson_2 files

---
+++ lesson_2/calculator.rb
@@ -0,0 +1,47 @@
+#
+# Simple Calculator
+#
+
+require 'yaml'
+MESSAGES = YAML.load_file('101/lesson_2/calculator_messages.yml')
+LANGUAGE = 'en'
+
+def messages(message, lang='en')
+  MESSAGES[lang][message]
+end
+
+def valid_expression?(expression)
+  regexp = %r{
+    \A\d+\.?\d* # any number that contains only one decimal
+    \s+         # white space after first number and before operand
+    [\+\/\-\*]  # matches available operands
+    \s+         # white space after first number and before operand
+    \d+\.?\d*$  # any number that contains only one decimal
+  }x
+
+  expression =~ regexp
+end
+
+def calculate(expression)
+  expression = expression.split()
+
+  values = [expression[0].to_f, expression[2].to_f]
+
+  operator = expression[1].to_sym
+
+  solution = values.inject(operator)
+
+  puts "#{messages('solution', LANGUAGE)} #{solution.round(2)}"
+end
+
+puts messages('welcome', LANGUAGE)
+loop do
+  puts messages('enter_expression', LANGUAGE)
+  expression = gets.chomp
+
+  if valid_expression?(expression)
+    calculate(expression)
+  else
+    puts messages('error_message', LANGUAGE)
+  end
+end
