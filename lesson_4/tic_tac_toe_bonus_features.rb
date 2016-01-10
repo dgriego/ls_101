51f28ffbe57dbf1af88db043a5035e21789574bd
dgriego <daniel@jackrabbitsystems.com>
2016-01-10 16:38:51 -0700

    adds routine for determining what player goes first improves game loop logic

--- lesson_4/tic_tac_toe_bonus_features.rb
+++ lesson_4/tic_tac_toe_bonus_features.rb
@@ -1,7 +1,7 @@
 # Tic Tac Toe Bonus Features
-
 COMPUTER_MARKER = 'O'
 PLAYER_MARKER = 'X'
+FIRST_PLAYER = 'choose'
 WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # horizontals
                 [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # verticals
                 [[1, 5, 9], [3, 5, 7]]              # diagonals
@@ -84,9 +84,9 @@ def computer_makes_choice(choices)
     end
   end
 
-  # Random Choice
   unless index
-    index = empty_positions(choices).sample
+    # choose middle square if it is empty before choosing random position
+    index = choices[5] == ' ' ? 5 : empty_positions(choices).sample
   end
 
   choices[index] = COMPUTER_MARKER
@@ -98,6 +98,27 @@ def joinor(choices_array, delimiter, word='or ') # => 1, 2, 3 or 4
   "#{choices_array.join(delimiter)}"
 end
 
+def place_piece!(choices, current_player)
+  if current_player == 'player'
+    player_makes_choice(choices)
+  else
+    computer_makes_choice(choices)
+  end
+end
+
+def alternate_player(current_player)
+  current_player == 'computer' ? 'player' : 'computer'
+end
+
+def determine_who_goes_first
+  if FIRST_PLAYER == 'choose'
+    puts 'Would you like to go first this Round?(y/n)'
+    gets.chomp.start_with?('n') ? 'computer' : 'player'
+  else
+    FIRST_PLAYER
+  end
+end
+
 player_score = 0
 computer_score = 0
 
@@ -105,11 +126,15 @@ loop do
   choices = {}
   (1..9).each { |i| choices[i] = " " }
 
+  system('clear')
+  puts 'Welcome to Tic Tac Toe!'
+  puts 'First to win 5 games wins'
+
+  current_player = determine_who_goes_first
+
   loop do
     system('clear')
 
-    puts 'Welcome to Tic Tac Toe!'
-    puts 'First to win 5 games wins'
     puts "Choose a position to place a piece #{joinor(choices.keys, ', ')}:"
     puts
 
@@ -118,15 +143,9 @@ loop do
     puts "Player Score: #{player_score} | " \
          "Computer Score: #{computer_score}"
 
-    # Player Turn
-    player_makes_choice(choices)
+    place_piece!(choices, current_player)
+    current_player = alternate_player(current_player)
     round_winner = check_for_round_win(choices)
-
-    # Computer Turn
-    unless round_winner
-      computer_makes_choice(choices)
-      round_winner = check_for_round_win(choices)
-    end
 
     if round_winner || empty_positions(choices).empty?
       system('clear')
